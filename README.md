7c5c602a69a3138070b248ac82093a3a04b45627
Tomasz Kłak <tomasz@tomaszklak.pl>
2024-12-01 06:30:19 +0100

    Day 1

--- README.md
+++ README.md
@@ -3,7 +3,10 @@
 # Advent of Code solutions 2024
 
 ```
+Day 01 took    61.2µs to compute (rsum    61.2µs) (with i/o:   176.2µs, rsum   176.2µs)
 
+         Total time for 1 days:    61.2µs (avg per day    61.2µs, med:    61.2µs, min:    61.2µs, max:    61.2µs)
+Total time with i/o for 1 days:   176.2µs (avg per day   176.2µs, med:   176.2µs, min:   176.2µs, max:   176.2µs)
 ```
 
 # previous solutions
--- src/day01.rs
+++ src/day01.rs
@@ -1,24 +1,43 @@
 use anyhow::Result;
+use rustc_hash::FxHashMap;
 use std::time::{Duration, Instant};
 
-use crate::input::tokens;
+use crate::{input::token_groups, vec::transpose_vec};
 
 pub fn solve(input: &str, verify_expected: bool, output: bool) -> Result<Duration> {
-    let lines: Vec<String> = tokens(input, None);
+    let lines: Vec<Vec<i64>> = token_groups(input, "\n", Some("   "));
 
     let s = Instant::now();
 
-    // TODO
+    let mut lists = transpose_vec(&lines);
+    let mut g1 = lists.swap_remove(0);
+    let mut g2 = lists.swap_remove(0);
+
+    g1.sort_unstable();
+    g2.sort_unstable();
+
+    let part1: i64 = g1.iter().zip(&g2).map(|(a, b)| (a - b).abs()).sum();
+
+    let mut counts: FxHashMap<i64, usize> = g1.iter().map(|i| (*i, 0)).collect();
+
+    for v in &g2 {
+        counts.entry(*v).and_modify(|v| *v += 1);
+    }
+
+    let part2: i64 = g1
+        .into_iter()
+        .map(|v| v * counts.get(&v).copied().unwrap_or_default() as i64)
+        .sum();
 
     let e = s.elapsed();
 
     if verify_expected {
-        // assert_eq!(0, part1);
-        // assert_eq!(0, part2);
+        assert_eq!(1223326, part1);
+        assert_eq!(21070419, part2);
     }
     if output {
-        // println!("\t{}", part1);
-        // println!("\t{}", part2);
+        println!("\t{}", part1);
+        println!("\t{}", part2);
     }
     Ok(e)
 }
