c2a3d6e78dff82a75ba13f23599d241049a1e90e
Tomasz Kłak <tomasz@tomaszklak.pl>
2024-12-02 07:01:32 +0100

    Clippy cleanups

--- src/vec.rs
+++ src/vec.rs
@@ -19,7 +19,7 @@ pub fn transpose_vec<T: Clone>(original: &[Vec<T>]) -> Vec<Vec<T>> {
     let mut transposed: Vec<Vec<T>> = vec![vec![]; original[0].len()];
 
     for original_row in original {
-        for (item, transposed_row) in original_row.into_iter().zip(&mut transposed) {
+        for (item, transposed_row) in original_row.iter().zip(&mut transposed) {
             transposed_row.push(item.clone());
         }
     }
