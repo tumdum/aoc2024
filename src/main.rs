8a8f9c27f1f709a8c14364ca5166f2b21810c93e
Tomasz Kłak <tomasz@tomaszklak.pl>
2024-12-18 06:43:05 +0100

    Day 18

--- src/main.rs
+++ src/main.rs
@@ -62,7 +62,7 @@ fn main() {
         &aoc24::day15::solve,
         &aoc24::day16::solve,
         &aoc24::day17::solve,
-        // &aoc24::day18::solve,
+        &aoc24::day18::solve,
         // &aoc24::day19::solve,
         // &aoc24::day20::solve,
         // &aoc24::day21::solve,
